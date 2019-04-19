e45635e3d41d5f3f718cb7aaaa04116b6b0cffac
Nikesh Bajaj <bajaj.nikkey@gmail.com>
2019-04-19 03:28:20 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+top_dir, _ = os.path.split(os.path.abspath(__file__))
+
+with open(os.path.join(top_dir, 'Version')) as f:
+    version = f.readline().strip()
+
+setuptools.setup(
+    name="spkit",
+    version= version,
+    author="Nikesh Bajaj",
+    author_email="nikkeshbajaj@gmail.com",
+    description="SpKit: Signal Processing toolkit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nikeshbajaj/spkit",
+    download_url = 'https://github.com/Nikeshbajaj/spkit/tarball/' + version,
+    packages=setuptools.find_packages(),
+    license = 'MIT',
+    keywords = 'Signal processing entropy Rényi entropy Kullback–Leibler divergence Mutual Information',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    install_requires=['numpy','matplotlib']
+)
